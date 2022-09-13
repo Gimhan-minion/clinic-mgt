8a13011a3317c90cb9c26bf3d040491daa6a2f78
Gimhan-minion <kavindrawijayawardana2000@gmail.com>
2022-09-13 18:04:26 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABC Clinic</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./index.css">
+</head>
+<body class="bg">
+    <?php include 'nav.php' ?>
+    <div class="main-container CardBgCol">
+    <div class="heading">
+        <h1 class="heading__title">ABC Clinic</h1>
+        <p class="heading__credits"><i>“Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet.” ― Lorem ipsum</i></p>
+    </div>
+    <div class="cards">
+        <div class="card card-1">
+            <h2 class="card__title"> Add New Account </h2>
+            <p class="card__apply">
+                <a class="card__link" href="#"> Click Here <i class="fas fa-arrow-right"></i></a>
+            </p>
+        </div>
+        <div class="card card-2">
+            <h2 class="card__title"> View Accounts </h2>
+            <p class="card__apply">
+                <a class="card__link" href="#">Click Here <i class="fas fa-arrow-right"></i></a>
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./index.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-md">
+    <a class="navbar-brand" href="index.php">ABC Clinic</a>
+    <div class="collapse navbar-collapse" id="main-navigation">
+        <ul class="navbar-nav" id="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Add New Account</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">View Accounts</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</body>
+</html>
